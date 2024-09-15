d9512e511485066b578a7b37f3d0d61200b9a34f
Marlonn Locatelli <marlonn@gmail.com>
2024-09-15 19:25:59 -0300

    Conversão do projeto para o uso do styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
+import GlobalStyle, { Container } from './global'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
-
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from 'react'
 
-import styles from './FormVagas.module.css'
+import { BotaoPesquisar, ClassStyle, InputStyle } from './styles'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +15,14 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <ClassStyle onSubmit={aoEnviarForm}>
+      <InputStyle
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <BotaoPesquisar type="submit">Pesquisar</BotaoPesquisar>
+    </ClassStyle>
   )
 }
 export default FormVagas
